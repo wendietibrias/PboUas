7d404b128e3915e7b6a637431c777cb4a9cee1e7
wendietibrias <wendietibrias@gmail.com>
2025-06-18 03:11:29 +0700

    feat: login

---
+++ src/login/LoginController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+import Koneksi.Koneksi;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import models.LoginModel;
+import models.RoleModel;
+import models.UserModel;
+
+/**
+ *
+ * @author Asus
+ */
+public class LoginController {
+    public Koneksi connection;
+    public Connection conn;
+    public Statement statement;
+    public ResultSet resultSet;
+    
+    public boolean handleLogin(LoginModel loginDto){
+        try {
+        connection = new Koneksi();
+        conn = connection.connection;
+        
+        statement = conn.createStatement();
+        
+        String checkLoginCredentialsQuery = "SELECT user.name,user.password,role.name as role_name" 
+                                                + " FROM user" 
+                                                + " LEFT JOIN role ON user.role_id = role.id"
+                                                + " WHERE user.name='"+loginDto.name+"' AND user.password='"+loginDto.password+"'";
+        
+        resultSet = statement.executeQuery(checkLoginCredentialsQuery);
+        
+        while(resultSet.next()){
+            String name = resultSet.getString("name");
+            String pass = resultSet.getString("password");
+            String role = resultSet.getString("role_name");
+            
+            System.out.println(name);
+            System.out.println(pass);
+            System.out.println(role);
+            
+            loginDto.setLoginCredential(name, pass, role);
+            loginDto.setIsLogged(true);
+        }
+         
+         resultSet.close();
+         statement.close();
+         conn.close();
+         
+         new UserModel().setUser(loginDto.name,loginDto.roleName);
+        
+         return loginDto.isLogged();
+         
+        } catch(Exception e){
+          System.out.println(e.getMessage());
+          return false;   
+        }
+      
+    }
+}
