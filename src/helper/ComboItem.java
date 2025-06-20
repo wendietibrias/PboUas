45a44cce5df3fd1b10f708e7582a9b72da9dc253
AlvinValentino <alvinvalentino986@gmail.com>
2025-06-20 19:29:27 +0700

    alvin commit

---
+++ src/helper/ComboItem.java
@@ -0,0 +1,29 @@
+package helper;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author deadg
+ */
+// ComboItem.java
+public class ComboItem {
+    private int id;
+    private String name;
+
+    public ComboItem(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
